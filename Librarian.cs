dffb61a1ffa89b3e10054dfc13a4ef806025a5a7
Ronik <ronik9@hotmail.co.uk>
2024-04-24 17:45:18 +0100

    Functionality to add, reove, checkout, check-in, list all and overdue books.

--- Librarian.cs
+++ Librarian.cs
@@ -2,53 +2,91 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Library
 {
-    public  class Librarian
+    public class Librarian
     {
 
         public Collection<Book> Books { get; set; }
         public Librarian()
         {
             Books = [
-                new Book(1, "Harry Potter and the Philosopher's Stone", "J.K.Rowling", true, DateTime.Now),
-                new Book(2, "Harry Potter and the Chamber of Secrets", "J.K.Rowling", true, DateTime.Now),
-                new Book(3, "Harry Potter and the Prisoner of Azkaban", "J.K.Rowling", true, DateTime.Now),
-                new Book(4, "Harry Potter and the Goblet of Fire", "J.K.Rowling", true, DateTime.Now),
-                new Book(5, "Harry Potter and the Order of the Phoenix", "J.K.Rowling", true, DateTime.Now),
-                new Book(6, "Harry Potter and the Half Blood Prince", "J.K.Rowling", true, DateTime.Now),
-                new Book(7, "Harry Potter and the Deathly Hallows", "J.K.Rowling", true, DateTime.Now)
+                new Book(0, "Harry Potter and the Philosopher's Stone", "J.K.Rowling", true, DateTime.Now),
+                new Book(1, "Harry Potter and the Chamber of Secrets", "J.K.Rowling", true, DateTime.Now),
+                new Book(2, "Harry Potter and the Prisoner of Azkaban", "J.K.Rowling", true, DateTime.Now),
+                new Book(3, "Harry Potter and the Goblet of Fire", "J.K.Rowling", true, DateTime.Now),
+                new Book(4, "Harry Potter and the Order of the Phoenix", "J.K.Rowling", true, DateTime.Now),
+                new Book(5, "Harry Potter and the Half Blood Prince", "J.K.Rowling", true, DateTime.Now),
+                new Book(6, "Harry Potter and the Deathly Hallows", "J.K.Rowling", true, DateTime.Now)
                 ];
 
         }
 
-        //public void AddBook(Book book)
-        //{
-        //    Books.Add(book);
-        //}
-
-        //public void RemoveBook(int id)
-        //{
-        //    Books.Remove(Books.Remove());
-        //}
-
-        public void CheckOut(int id)
+        public void AddBook(Book book)
         {
-
+            Books.Add(book);
+            Console.WriteLine("You have successfully added " + book.Title + " by " + book.Author + " to the library.");
         }
 
-        public void Checkin(int id)
+        public bool RemoveBook(int id)
         {
+            if (IsIdValid(id))
+            {
+                var title = Books[id].Title;
+                var author = Books[id].Author;
+                Books.Remove(Books[id]);
+                ReassignIds();
+                Console.WriteLine("You have successfully removed " + title + " by " + author + " from the library.");
 
+                return true;
+            }
+            return false;
         }
 
-        //public Collection<Book> CalculateOverdueBooks()
-        //{
+        public bool CheckOut(int id)
+        {
+            if (IsIdValid(id) && Books[id].Available)
+            {
+                Books[id].Available = false;
+                Books[id].CheckInOutTime = DateTime.Now;
+                Console.WriteLine("You have successfully checked out " + Books[id].Title + " by " + Books[id].Author + ".");
+                return true;
+            }
+            return false;
+        }
 
-        //}
+        public bool CheckIn(int id)
+        {
+            if (IsIdValid(id) && !Books[id].Available)
+            {
+                Books[id].Available = true;
+                Books[id].CheckInOutTime = DateTime.Now;
+                Console.WriteLine("You have successfully checked in " + Books[id].Title + " by " + Books[id].Author + ".");
+                return true;
+            }
+            return false;
+        }
+
+        public Collection<Book> CalculateOverdueBooks()
+        {
+             return [.. Books.Where(book => !book.Available && ((DateTime.Now - book.CheckInOutTime).TotalMinutes > 1))];  
+        }
+        public bool IsIdValid(int id)
+        {
+            return id >= 0 && id < Books.Count;
+        }
+
+        public void ReassignIds()
+        {
+            foreach(Book book in Books)
+            {
+                book.Id = Books.IndexOf(book);
+            }
+        }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,42 +2,172 @@
 using Library;
 
 Librarian librarian = new Librarian();
+Console.WriteLine("Nau mai haere mai i roto te library!");
+InstructUser();
+ServeUser();
 
-Console.WriteLine("Nau mai haere mai i roto te library!" +
-    " Press 'A' to add a book. " +
-    "Press 'R' to remove a book. " +
-    "Press 'I' to check in a book." +
-    "Press 'O' to check out a book" +
-    "Press 'S' to see all books" +
-    "Press 'L' to see all overdue books");
-
-
-var input = Convert.ToChar(Console.ReadKey());
-switch (Char.ToLower(input))
+void ServeUser()
 {
-    //case A:
-    //    break;
+    Console.WriteLine();
+    var input = Convert.ToChar(Console.ReadKey().Key);
+    Console.WriteLine();
+    switch (Char.ToLower(input))
+    {
+         case 'a':
+            AddBook();
+            ServeUser();
+            break;
 
-    //case R:
-    //    break;
-    //case I:
-    //    break;
+        case 'r':
+            RemoveBook();
+            ServeUser();
+            break;
 
-    //case O:
-    //    break;
-    case 's':
-        foreach (Book book in librarian.Books)
-        {
-            Console.WriteLine(book);
-        }
-        break;
+        case 'i':
+            ServeCheckIn();
+            break;
 
-    //case L:
-    //    break;
+        case 'o':
+            ServeCheckout();
+            break;
 
-    default: Console.WriteLine("Please Select a valid option");
-        break;
+        case 's':
+            ListBooks();
+            ServeUser();
+            break;
+
+        case 'l':
+            ListOverdueBooks();
+            ServeUser();
+            break;
+
+        default:
+            InstructUser();
+            ServeUser();
+            break;
+    }
+}
+void InstructUser()
+{
+    Console.WriteLine("Press 'A' to add a book. ");
+    Console.WriteLine("Press 'R' to remove a book. ");
+    Console.WriteLine("Press 'I' to check in a book.");
+    Console.WriteLine("Press 'O' to check out a book");
+    Console.WriteLine("Press 'S' to see all books");
+    Console.WriteLine("Press 'L' to see all overdue books");
 }
 
+void ServeCheckout()
+{
+    Console.WriteLine("Select an Id to checkout a book:");
+    var userInput = Console.ReadKey();
+    var id = Char.IsDigit(userInput.KeyChar) ? int.Parse(userInput.KeyChar.ToString()) : -1;
+    Console.WriteLine();
+    if (librarian.CheckOut(id))
+    {
+        ServeUser();
+    }
+    else
+    {
+        Console.WriteLine("Please select a valid id and an available book.");
+        ServeCheckout();
+    }
+}
 
+void ServeCheckIn()
+{
+    Console.WriteLine("Select an Id to checkin a book:");
+    var userInput = Console.ReadKey();
+    var id = Char.IsDigit(userInput.KeyChar) ? int.Parse(userInput.KeyChar.ToString()) : -1;
+    Console.WriteLine();
+    if (librarian.CheckIn(id))
+    {
+        ServeUser();
+    }
+    else
+    {
+        Console.WriteLine("Please select a valid id and an unavailable book.");
+        ServeCheckIn();
+    }
+}
 
+void ListBooks()
+{
+    Console.WriteLine("Books:");
+    foreach (Book book in librarian.Books)
+    {
+        Console.WriteLine("Id: " + book.Id + " Title: " + book.Title + ", Author: " + book.Author + ", Available: " + book.Available);
+
+    }
+}
+
+void AddBook()
+{
+    librarian.AddBook(new Book(librarian.Books.Count, AddTitle(), AddAuthor(), true, DateTime.Now));
+}
+
+string AddTitle()
+{
+    Console.WriteLine("Enter the title of the book you would like to add to the library");
+    var title = Console.ReadLine();
+    Console.WriteLine("Press 'Y' to confirm the title and 'N' to rewrite the title.");
+    var confirm = Convert.ToChar(Console.ReadKey().Key);
+    Console.WriteLine();
+    switch (Char.ToLower(confirm))
+    {
+        case 'y':
+            return title;
+
+        case 'n':
+            return AddTitle();
+
+        default: return title;
+    }
+}
+
+string AddAuthor()
+{
+    Console.WriteLine("Enter the author of the book you would like to add to the library");
+    var author = Console.ReadLine();
+    Console.WriteLine("Press 'Y' to confirm the author and 'N' to rewrite the author.");
+    var confirm = Convert.ToChar(Console.ReadKey().Key);
+    Console.WriteLine();
+    switch (Char.ToLower(confirm))
+    {
+        case 'y':
+            return author;
+
+        case 'n':
+            return AddAuthor();
+            break;
+
+        default: return AddAuthor();
+    }
+}
+
+void RemoveBook()
+{
+    Console.WriteLine("Select an Id to remove a book from the library:");
+    var userInput = Console.ReadKey();
+    var id = Char.IsDigit(userInput.KeyChar) ? int.Parse(userInput.KeyChar.ToString()) : -1;
+    Console.WriteLine();
+    if (librarian.RemoveBook(id))
+    {
+        ServeUser();
+    }
+    else
+    {
+        Console.WriteLine("Please select a valid id to remove a book from the library.");
+        RemoveBook();
+    }
+}
+
+void ListOverdueBooks()
+{
+    Console.WriteLine("Overdue Books:");
+    foreach (Book book in librarian.CalculateOverdueBooks())
+    {
+        Console.WriteLine("Id: " + book.Id + "Title: " + book.Title + ", Author: " + book.Author + ", Available: " + book.Available + "Checkout: " + book.CheckInOutTime);
+
+    }
+}
